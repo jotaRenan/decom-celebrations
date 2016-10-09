f5a4ed71d2063ec6b65e74bbd1d29389fcbdc72c
Paula Ribeiro <paula.mribeiro05@gmail.com>
2016-10-09 17:07:13 -0300

    começa a implementar bd

--- Game.js
+++ Game.js
@@ -259,7 +259,9 @@ BasicGame.Game.prototype = {
     //  Stop music, delete sprites, purge caches, free resources, all that good stuff.
     console.log('jogo terminou');
     let tempoFinal = this.time.elapsedSecondsSince(this.inicio).toFixed(3);
-    console.log(tempoFinal + ' ');
+    //console.log(tempoFinal);
+    var nome = prompt("Insira aqui o seu nome:", "Adalbinho");
+    //console.log(nome);
     //  Then let's go back to the main menu.
     
     this.state.start('MainMenu');
@@ -278,6 +280,30 @@ BasicGame.Game.prototype = {
       }
     }
   },
+
+  atualizaRanking: function(tempo, nome) {
+    var token = "b01d31a1-8b9f-4f8f-814e-54dcb8c7aff8";
+    let db = require('orquestrate')(token);
+    db.put('tempos',  {
+      'nome' : nome,
+      'tempo' : tempo
+    })
+    .then(function (res) {
+      console.log(res); // prints response
+    })
+    .fail(function (err) {
+      console.log(err); // prints error
+    });
+
+    db.get('tempos', 'tempo', 'nome')
+    .then(function (res) {
+      console.log(res.body); // prints response body
+      console.log(res.body.name); // prints 'Bruce Wayne'
+    })
+    .fail(function (err) {
+      console.log(err); // prints error
+    });
+  }
 
 
 };
